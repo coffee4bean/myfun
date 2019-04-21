e1784c4878f990ef15f73414947e2ed8fae7f984
coffee4bean <15586698775@163.com>
2019-04-21 21:22:41 +0800

    好像可以提交了

---
+++ src/main/java/com/test/App.java
@@ -0,0 +1,14 @@
+package com.test;
+
+
+public class App {
+    private App app= new App();
+
+    public App() {
+        System.out.println("测试");
+    }
+
+    public static void main(String[] args) {
+        new App();
+    }
+}
